33cc447f9fb95bd7b270d289ab85742b586e54d0
jsfelipe <jsfelipe@gmail.com>
2013-11-06 22:49:51 -0300

    salvar, editar, busca
    salvar, editar, busca. letras no menu esquerdo

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,13 +1,50 @@
 function AgendaCtrl($scope, $http, $window){
-		
-	$scope.salvar = function(addWine){
-		$http.post('service/wines', $scope.contato).success(function(data){
-			$scope.contatos.unshift(data);
-			$scope.formCadastro.$setPristine();
-		});
-	}
+	
+	$ = jQuery;
+	$scope.letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 	
+	$scope.passaLetra = function(letra){
+		//$scope.letra = letra;
+		//$scope.contat = {id:0, nome:'teste', email:''};
+		console.log($scope.contato.nome);
+	}
+
+
+	$scope.salvar = function(){
+
+		if($scope.contato.id > 0){
+
+			$http.put('service/wines/'+$scope.contato.id, $scope.contato).success(function(data){
+				$(".alert").css({display: 'block'});
+				$(".alert").addClass('alert-warning');
+				$("#msg_alert").html('Alterado com sucesso!');
+			
+			});
+
+		}else{
+
+			$http.post('service/wines', $scope.contato).success(function(data){
+				$scope.contatos.unshift(data);
+				$(".alert").css({display: 'block'});
+				$(".alert").addClass('alert-success');
+				$("#msg_alert").html('Cadastrado com sucesso!');
+				
+			});
+
+		}
+		$('#modalCadastro').modal('hide');
+		$(".alert").css({display: ''}).fadeOut(5000);
+	}
+
+	$scope.visualizar = function(contato){
+		$scope.contato = contato;
+	}
+
+	$scope.editar = function(){
+		$('#modalCadastro').show();
+	}
+
 
 	var init = function(){
 		
